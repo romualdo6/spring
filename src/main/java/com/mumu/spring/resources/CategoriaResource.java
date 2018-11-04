d9cd40c8338b9868d1edd28afd1cb7c5a55e31f5
Romualdo Duarte <romualdo_6@msn.com>
2018-11-04 09:37:42 -0200

    Testando classe de domain

--- src/main/java/com/mumu/spring/resources/CategoriaResource.java
+++ src/main/java/com/mumu/spring/resources/CategoriaResource.java
@@ -1,14 +1,28 @@
 package com.mumu.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mumu.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Works";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 }
